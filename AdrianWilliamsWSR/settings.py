114bac6317d2eb7384dd04a12a5123187e9ab87a
Akshat Jain <akshatt.jain9@gmail.com>
2023-07-05 12:20:21 +1000

    Added Basic Login Page

--- AdrianWilliamsWSR/settings.py
+++ AdrianWilliamsWSR/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.home.apps.HomeConfig',
+    'apps.login.apps.LoginConfig',
 ]
 
 MIDDLEWARE = [
--- AdrianWilliamsWSR/urls.py
+++ AdrianWilliamsWSR/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include("apps.home.urls")),
+    path('', include("apps.login.urls")),
 ]
