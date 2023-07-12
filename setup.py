2777474cb6471f28e619e68fc772174e92df4fcc
Akshat Jain <akshatt.jain9@gmail.com>
2023-07-12 22:36:10 +1000

    Try Different setup

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 import os
 from setuptools import setup
 
-def read_requirements():
-    with open('Pipfile.lock') as f:
-        requirements = f.readlines()
-    return [req.strip() for req in requirements]
+# Read the contents of the Pipfile.lock file
+pipfile_lock = os.path.join(os.path.dirname(__file__), 'Pipfile.lock')
+requirements = []
+if os.path.exists(pipfile_lock):
+    import json
+    with open(pipfile_lock) as f:
+        pipfile_data = json.load(f)
+        if 'default' in pipfile_data and 'packages' in pipfile_data['default']:
+            requirements = [package_name + package_data['version'] for package_name, package_data in pipfile_data['default']['packages'].items()]
 
+# Perform the setup
 setup(
     name='adrian-william-wsr',
     version='1.0',
-    install_requires=read_requirements(),
+    description='Weekly Service Report App',
+    author='Adrian William',
+    install_requires=requirements,
+    packages=['adrian-william-wsr'],
 )
+
